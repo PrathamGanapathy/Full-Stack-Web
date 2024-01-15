f5d06028bd272c73c728883ed0fad5a0022382c8
PrathamGanapathy <pratham.cs22@bmsce.ac.in>
2024-01-15 19:45:35 +0530

    new file:   test_routine/Cardio/Cardio.html 	new file:   test_routine/Cardio/ExternalCardio.js 	new file:   test_routine/Cardio/StyleCardio.css 	modified:   test_routine/Routine_Basketball.html

---
+++ test_routine/Cardio/ExternalCardio.js
@@ -0,0 +1,97 @@
+//Neck stretches
+function Completed1(){
+    var value=document.getElementById('Complete1')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('1').classList.add('highlight');
+    }
+}
+
+//Shoulder rolls
+function Completed2(){
+    var value=document.getElementById('Complete2')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('2').classList.add('highlight');
+    }
+}
+
+//Arm Circles
+function Completed3(){
+    var value=document.getElementById('Complete3')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('3').classList.add('highlight');
+    }
+}
+//Triceps Stretch
+function Completed4(){
+    var value=document.getElementById('Complete4')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('4').classList.add('highlight');
+    }
+}
+//Wrist Stretches
+function Completed5(){
+    var value=document.getElementById('Complete5')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('5').classList.add('highlight');
+    }
+}
+//Chest Opener
+function Completed6(){
+    var value=document.getElementById('Complete6')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('6').classList.add('highlight');
+    }
+}
+//Cat-Cow
+function Completed7(){
+    var value=document.getElementById('Complete7')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('7').classList.add('highlight');
+    }
+}
+//Hip Flexor Stretch
+function Completed8(){
+    var value=document.getElementById('Complete8')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('8').classList.add('highlight');
+    }
+}
+//Quadricep Stretches
+function Completed9(){
+    var value=document.getElementById('Complete9')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('9').classList.add('highlight');
+    }
+}
+//Calf Stretches
+function Completed10(){
+    var value=document.getElementById('Complete10')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('10').classList.add('highlight');
+    }
+}
+
+//Back button to go back to Routines page
+function Back(){
+    window.location.href='Routine_Basketball.html';
+}
