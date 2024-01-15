3f9a2650274fc25e4117f3e7732de1dbf351f401
PrathamGanapathy <pratham.cs22@bmsce.ac.in>
2024-01-15 16:53:26 +0530

    new file:   test_routine/exercises/Ankel.html 	new file:   test_routine/exercises/Arm.html 	new file:   test_routine/exercises/Calf.html 	new file:   test_routine/exercises/Cat.html 	new file:   test_routine/exercises/Chest.html 	new file:   test_routine/exercises/External.js 	new file:   test_routine/exercises/Hip.html 	new file:   test_routine/exercises/Leg.html 	new file:   test_routine/exercises/Quads.html 	new file:   test_routine/exercises/Shoulder.html 	modified:   test_routine/exercises/Stretches.html 	deleted:    test_routine/exercises/Stretches2.html 	new file:   test_routine/exercises/Triceps.html 	new file:   test_routine/exercises/Wrist.html

---
+++ test_routine/exercises/External.js
@@ -0,0 +1,119 @@
+//Neck stretches
+function Completed1(){
+    var value=document.getElementById('Complete1')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('1').classList.add('highlight');
+    }
+}
+
+//Shoulder rolls
+function Completed2(){
+    var value=document.getElementById('Complete2')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('2').classList.add('highlight');
+    }
+}
+
+//Arm Circles
+function Completed3(){
+    var value=document.getElementById('Complete3')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('3').classList.add('highlight');
+    }
+}
+//Triceps Stretch
+function Completed4(){
+    var value=document.getElementById('Complete4')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('4').classList.add('highlight');
+    }
+}
+//Wrist Stretches
+function Completed5(){
+    var value=document.getElementById('Complete5')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('5').classList.add('highlight');
+    }
+}
+//Chest Opener
+function Completed6(){
+    var value=document.getElementById('Complete6')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('6').classList.add('highlight');
+    }
+}
+//Cat-Cow
+function Completed7(){
+    var value=document.getElementById('Complete7')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('7').classList.add('highlight');
+    }
+}
+//Hip Flexor Stretch
+function Completed8(){
+    var value=document.getElementById('Complete8')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('8').classList.add('highlight');
+    }
+}
+//Quadricep Stretches
+function Completed9(){
+    var value=document.getElementById('Complete9')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('9').classList.add('highlight');
+    }
+}
+//Calf Stretches
+function Completed10(){
+    var value=document.getElementById('Complete10')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('10').classList.add('highlight');
+    }
+}
+//Ankel Roll
+function Completed11(){
+    var value=document.getElementById('Complete11')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('11').classList.add('highlight');
+    }
+}
+//Leg Swings
+function Completed12(){
+    var value=document.getElementById('Complete12')
+    if(value.value=='NC'){
+        value.value='C';
+        value.innerHTML="Completed";
+        document.getElementById('12').classList.add('highlight');
+    }
+}
+
+
+
+
+
+
+function Back(){
+    window.location.href='Routine_test4.html';
+}
